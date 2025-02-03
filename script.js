287a9bf207522171f1b0605c6d9d0e404bf7966c
Dragon-Chicken <ethandsouza256@gmail.com>
2025-02-03 14:46:34 +0530

    added some things added flexbox for the top bar made the pages like wikipedia

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function loadHTML(elementId, fileName) {
 // Load the header and footer from the respective HTML files
 loadHTML("header", "/header.html");
 loadHTML("footer", "/footer.html");
+
+const observer = new MutationObserver((mutations) => {
+  const button = document.getElementById('hamburger');
+  const paragraph = document.querySelector("#testing");
+  
+  if (button) {
+    button.addEventListener("click", updateButton);
+    console.log("button is found!", button);
+    observer.disconnect();
+  }
+
+  function updateButton() {
+    if (button.value == "start x") {
+      console.log("button pressed");
+      button.value = "stop x";
+      paragraph.textContent = "x is started";
+    } else {
+      console.log("button unpressed");
+      button.value = "start x";
+      paragraph.textContent = "x is stopped";
+    }
+  }
+
+});
+observer.observe(document.body, { childList: true, subtree: true });
